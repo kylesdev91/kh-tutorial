0aeac295ba874ca155dffd4c6d1feb999d735d30
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-20 11:55:37 -0600

    final

--- src/store/user.js
+++ src/store/user.js
@@ -1,29 +1,29 @@
 import { defineStore } from "pinia";
-import { v4 as uuid } from "uuid"
+import { v4 as uuid } from "uuid";
 
-export const useUserStore = defineStore('users', {
-    state: () => {
-      return {
-        users: [],
-      }
+export const useUserStore = defineStore("users", {
+  state: () => {
+    return {
+      users: [],
+    };
+  },
+  actions: {
+    create(user) {
+      this.users.push({
+        id: uuid(),
+        ...user,
+      });
     },
-    actions: {
-      create(user) {
-        this.users.push({
-          id: uuid(),
-          ...user,
-        });
-      },
-      delete(id) {
-        this.users = this.users.filter((user) => user.id !== id);
-      }
+    delete(id) {
+      this.users = this.users.filter((user) => user.id !== id);
     },
-    getters: {
-      userName(state) {
-        const sortable = [...state.users];
-        return sortable.sort((a, b) => {
-            return a.name.localeCompare(b.name)
-        })
-      }
-    }
-})
+  },
+  getters: {
+    usersByName(state) {
+      const sortable = [...state.users];
+      return sortable.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    },
+  },
+});
